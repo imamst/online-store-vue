5a9d30f4d8f0df3e5c1b66f622ba923707cd2e42
Imam Setiawan <s.imam29@gmail.com>
2022-05-27 12:37:01 +0700

    setup tailwind ui, router & pinia

---
+++ src/store/category.js
@@ -0,0 +1,7 @@
+import { defineStore } from "pinia"
+
+const useCategory = defineStore({
+    id: "category"
+})
+
+export default useCategory
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      gridTemplateRows: {
+        '[auto,auto,1fr]': 'auto auto 1fr',
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
